b781461819a7f16ea6be49b8ec54739ef295ce21
www-data <www-data@s5132848.elf.ict.griffith.edu.au>
2019-08-07 12:00:22 +1000

    Start A1

---
+++ assessments/A1/resources/views/homeForm.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Social Media Application</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="{{asset('css/wp.css')}}">
+</head>
+
+<body>
+
+<ul class="navbar">
+    <li><a class="active" href="">Home</a></li>
+    <li><a href="">Most Recent</a></li>
+    <li><a href="">Unique</a></li>
+</ul>
+
+
+</body>
