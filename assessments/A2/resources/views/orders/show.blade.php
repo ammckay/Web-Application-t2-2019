285faa2017ebc5afe389a2170d77cf3fa0c19ef7
www-data <www-data@s5132848.elf.ict.griffith.edu.au>
2019-09-27 21:42:51 +1000

    Orders address working

--- assessments/A2/resources/views/orders/show.blade.php
+++ assessments/A2/resources/views/orders/show.blade.php
@@ -8,7 +8,7 @@
     <p>Name: {{$order->user_name}}</p>
     <p>Dish: {{$order->product_name}}</p>
     <p>Price: ${{$order->price}}</p> 
-    <p>Address: {{$order->user_address}}</p>
+    <p>Address: {{$order->address}}</p>
     
     <p><a href='{{url("product")}}'>Restaurants</a></p> 
 
